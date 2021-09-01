c3e5ca31a618c8233c184aa3ab6925a24390dac4
Susan Ortiz <suu.ortizm@gmail.com>
2021-09-01 13:00:25 -0400

    se modifica formulario y boton de html

--- src/index.js
+++ src/index.js
@@ -7,11 +7,21 @@ boton.addEventListener("click", function () {
   console.log(validator.isValid(numeroTarjeta));
 });
 
+//para girar boton y para que abra el formulario//
 const btnAbrirForm = document.getElementById("btnAbrirForm");
 const formulario = document.getElementById("formularioTarjeta");
 btnAbrirForm.addEventListener("click", function () {
   btnAbrirForm.classList.toggle("active");
   formulario.classList.toggle("active");
+});
+
+//Input numero de tarjeta//
+//la e en function es para recibir el evento
+//target.value se accede al valor del input numerotarjeta
+formulario.numeroTarjeta.addEventListener("keyup", function (e) {
+  let valorInput = e.target.value; //aqui se guarda el valor ingresado
+  formulario.numeroTarjeta.value = valorInput.replace();
+  //para pasar una expresion regular
 });
 
 console.log(validator);
